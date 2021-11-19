74d4e049375eb75375b2eb2fd2a3e05b97cb7a4d
cesar <rodbc99@gmail.com>
2021-11-19 00:10:16 -0300

    tercera Parte Homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -86,6 +86,12 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  // return num < 90
+  if (num < 90) {
+    return true 
+  } else {
+    return false 
+  }
   
 }
 
